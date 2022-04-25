d9d5c9b58b7cd977cf0ee599190d8977e40c6d9e
MathXCruz <matheus.cruz1996@hotmail.com>
2022-04-25 14:32:35 -0300

    updated coverage

--- tests/test_poke_api.py
+++ tests/test_poke_api.py
@@ -1,6 +1,6 @@
 import pytest as pt
-from poke_api.extract import get_data
-from poke_api.transform import summarize, parse_dict
+from poke_api.extract import get_info
+from poke_api.transform import summarize
 from poke_api import __version__
 
 
@@ -10,6 +10,6 @@ def test_version():
 
 def test_summarize():
     assert (
-        summarize(get_data('pokemon', '1'))
+        summarize(get_info('pokemon', '1'))
         == 'Bulbasaur (ID: 1) is a grass/poison type with a weight of 6.90Kg and a height of 0.70m. It looks like this: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-    )
+    ) 
